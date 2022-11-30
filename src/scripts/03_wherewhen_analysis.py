df74a3088e4182d376b1b241a62f782757de7ac8
alessiogandelli <alessiogandelli99@gmail.com>
2022-11-30 17:24:32 +0100

    add app

--- src/scripts/03_wherewhen_analysis.py
+++ src/scripts/03_wherewhen_analysis.py
@@ -10,54 +10,13 @@ db = Database('/Volumes/boot420/Users/alessiogandelli/data_social_dynamics/aaa.d
 with open('/Users/alessiogandelli/dev/uni/attention-analysis/data/userids.json') as f:
     userids = json.load(f)['userid_ww']
 
-# %%
 
-query = "select userid, date, time, timestamp ,what, where2, withwho from diary where userid in {} ".format(tuple(userids))
-
-df = pd.read_sql(query, db.connection)
-
-df.value_counts('what')
-# change 
-
-
-
-# %%
-df = df[df['date'] == '2020-11-20'].groupby(['timestamp', 'what']).size().unstack(fill_value = 0)
-
-# %%
-df.plot(kind = 'area', stacked = True, figsize = (20, 10), title = 'Diary events per minute')
-# %%
-# %%
-
-# %%
-df.index = pd.to_datetime(df.index)
-df.plot(kind = 'area', stacked = True, figsize = (20, 10), title = 'Diary events per minute').legend(loc='center left',bbox_to_anchor=(1.0, 0.5));
-plt.xticks(rotation = 90)
-#add one tick per hour 
-#plt.xticks(ticks = df.index[::60], labels = df.index[::60])
-
-
-
-
-
-#%%
-
-query = '''SELECT diary.userid, year, month, day, hour, min, diary.what 
-            FROM yd
-            LEFT JOIN diary 
-            ON  yd.year = strftime('%Y', diary.timestamp) AND
-                yd.month = strftime('%m', diary.timestamp) AND
-                yd.day = strftime('%d', diary.timestamp) AND
-                yd.hour = strftime('%H', diary.timestamp) AND
-                yd.userid = diary.userid order by diary.timestamp'''
-
-df = pd.read_sql(query, db.connection)
 # %%
 
 # INIZIA DA QUI ####################################
 
 
-
+''' get yardstick'''
 
 query = "select * from yd where userid in {} ".format(tuple(userids))
 df_yd = pd.read_sql(query, db.connection)
@@ -66,7 +25,7 @@ df_yd = df_yd[['userid', 'day', 'hour', 'min']]
 df_yd = df_yd.astype({'hour': 'int64', 'min': 'int64', 'day': 'datetime64[ns]'})
 
 
-
+''' get time diary  '''
 
 query = "select * from diary where userid in {} ".format(tuple(userids))
 df_diary = pd.read_sql(query, db.connection)
@@ -100,6 +59,21 @@ df_touch = df_touch.astype({'hour': 'int64', 'min': 'int64', 'day': 'datetime64[
 
 
 #%%
+
+query = '''select  app.userid,
+        date(app.timestamp) as day,  
+        strftime('%H', app.timestamp) as hour, 
+        strftime('%M', app.timestamp) as min, 
+        application as apps 
+        from application as app
+
+    group by app.userid, 
+            date(app.timestamp), 
+            strftime('%H', app.timestamp), 
+            strftime('%M', app.timestamp)'''
+
+df_app = pd.read_sql(query, db.connection)
+df_app = df_app.astype({'hour': 'int64', 'min': 'int64', 'day': 'datetime64[ns]'})
 # drop every column except timestamp userid and what 
 
 
@@ -107,7 +81,7 @@ df_touch = df_touch.astype({'hour': 'int64', 'min': 'int64', 'day': 'datetime64[
 # join tables 
 df = df_yd.merge(df_diary, on = ['userid','day', 'hour', 'min'], how = 'left')
 df = df.merge(df_touch, on = ['userid','day', 'hour', 'min'], how = 'left')
-
+df = df.merge(df_app, on = ['userid','day', 'hour', 'min'], how = 'left')
 
 df = df.sort_values(['userid','day', 'hour', 'min'])
 df['id'] = range(len(df))
@@ -117,10 +91,22 @@ df['touches'] = df['touches'].fillna(0).astype('int64')
 
 
 
-# %%
-## fillna method ffill 
-df['what'] = df['what'].fillna(method = 'ffill')
+df = df[df['apps'].str.contains('launcher') == False]
+df = df[df['apps'] != '']
+df = df[df['apps'] != 'com.miui.home' ]
+df = df[df['apps'] != 'it.unitn.disi.witmee.sensorlog' ]
 
-#set max rows to 1000
-pd.set_option('display.max_rows', 1000)
+
+
+
+
+# %%
+gp = df.groupby(['what','apps'])['touches'].count()
+gp = gp.reset_index().sort_values(['what','touches'], ascending = False)
+
+# drop rows where app is launcher 
+
+
+# get top three for each what
+gp = gp.groupby('what').head(3)
 # %%
