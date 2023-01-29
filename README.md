636aef5ff27be703fcf211176b57eb1272783da6
alessiogandelli <alessiogandelli99@gmail.com>
2023-01-29 17:29:04 +0100

    recap

--- README.md
+++ README.md
@@ -11,10 +11,9 @@ data have been collected in november-december 2020
 In 2020 all the unitn students were asked to participate in this study. The sample is composed by unitn students, mainly italians, android users that accepted to participate in the study under a monetary compensation.
 
 
-
 more information about the data collection [here](https://www.internetofus.eu/wp-content/uploads/sites/38/2021/08/2021-Datascientia-LivePeople-WeNet2020.pdf)
 
-the data have been collected mainly in 3 ways and a lot of data have been collected, i'll talk only about the data that i used in this project.
+the data have been collected mainly in 3 ways and for each students we have a lot of data, i'll talk only about the data that i used in this project.
 
 ## questionnaires 
 socio demographic data such as departement, age, sex  and psychological data: diffrent indicators but i only consider big 5 personality traits 
@@ -78,7 +77,7 @@ CREATE TABLE IF NOT EXISTS "diary" (
 
 
 # data preparation  
-Here i describe the processof data preparation, starting from the raw data, passinf to the cleaning and ending with the final dataset. The demographic and the time diary were already cleaned while the sesors data wer not so i had to do it by myself. 
+Here i describe the process of data preparation, starting from the raw data, passing to the cleaning and ending with the final dataset. The demographic and the time diary were already cleaned while the sesors data wer not so i had to do it by myself.
 
 
 ## data import to sqlite
@@ -121,7 +120,7 @@ the next step was to aggregate the data in order to have a more compact represen
 
 we start looking into general statistics on how students use their phones, the most used apps and the top installed and the top notification
 
-then i looked at an aggregated time diary to see what people do  during the day and turns out yhat they spend most of their time sleeping 20% and on average 11% studying 
+then i looked at an aggregated time diary to see what people do during the day and turns out that the  longest activity is sleeping 20% and on average 11% studying 
 
 now i want to focus on what people do when they study and see if i can group in different clusters and correlate with perdonality trait or demographics 
 
@@ -129,18 +128,39 @@ now i want to focus on what people do when they study and see if i can group in 
 
 the first thing was to look at the self reported time diary and to remove outliers, i.e. people that reported they studied 100% of the time (1 user) and 0% (4 users)
 
-we also have to take into account the period, in fact we are between november and december and how people study is not homegenous during the semester, i expect the ones with preappelli in december study more than the others that have exams in january and febraury, since we do not have this information we have to be careful in the interpretation of the results, in fact if someone attending a spefic course is studyng more than other we cannot say that that course is more difficult or require ore effort but maybe there are different deadlines, for such analysis we need morelongitudinal data
+we also have to take into account the period, in fact we are between november and december and how people study is not homegenous during the semester, I expect the ones with 'preappelli' in december study more than the others that have exams in january and febraury, since we do not have this information we have to be careful in the interpretation of the results, in fact if someone attending a spefic course is studyng more than other we cannot say that that course is more difficult or require ore effort but maybe there are different deadlines, for such analysis we need more longitudinal data
 
-said that now i want to study the use of the phone during the study time, i.e. what people do when they study, i.e. what apps they use, how many notifications they receive, how many touches they do, etc.
+said that now i want to study the use of the phone during the study time, i.e. what people do when they study: 
+-  what apps they use
+- how many notifications they receive
+- how many touches they do, etc.
 
-the first metric i want to see is how frequently people touch their phone when they study, it turns out that there are few people that can study without touching their phones but most of them can't stay without more than 20 minutes. 
+the first metric I explore is  how frequently people touch their phone when they study, it turns out that there are few people that can study without touching their phones but most of them can't stay without more than 20 minutes. 
 the mean is 25 min but the median is 5, this mean that probably sometimes they do no touch the phone for a long time and this increase the average 
 
-then it is interesting to see which application are they using , because some of them may are using the calculator but others whstapp, we'll also see if the touch of the phone is related to a notification 
+then it is interesting to see which application are they using, because some of them may are using the calculator but others whastapp, we'll also see if the touch of the phone is related to a notification.
 
 
+### applications 
+Looking at this image we can see how with the first 4 apps ( whatsapp, instagram, youtube, chrome) we cover 50% of the time, then we have a long tail of apps that are used less than 1% of the time, this is not surprising since we have 1000 apps installed and we use just a few of them. 
+
+![top apps](/plots/top_apps.png)
+
+Looking at the most installed apps we can see the presence of gmail, which is not a very used app but today checking mail is necessary, taking into account akso that the university mail is a google account.
+
+![top downloaded apps](/plots/top_installed_apps.png)
+
+### notifications
+Looking at notifications the presence of whatsapp is clear, but at the second place there is telegram which is not on the top download, but it is a very popular app, especially among students, where there are big groups and so receiveing many notification is normal. 
+
+![top notifications](/plots/top_notification_tree.png)
 
 
+### what 
+In this figure we can see for each hour what people do 
+
+
+![what](/plots/what.png)
 
 
 
--- src/scripts/03_analysis/04_study.py
+++ src/scripts/03_analysis/04_study.py
@@ -12,26 +12,22 @@ plot_folder = '/Users/alessiogandelli/dev/uni/attention-analysis/plots/'
 #%%
 
 ''' LOAD DATA '''
+#df.to_pickle(data_folder + 'study_analysis.pkl')
 
-types = {'what': 'category', 'apps': 'category', 'age' : 'int64', 'gender' : 'category', 'nationality' : 'category' , 'department': 'category', 'degree' : 'category', 'notification': 'category', 'what' : 'category', 'apps' : 'category', }
-df = pd.read_csv(data_folder + 'merged_data.csv', dtype= types, parse_dates=['day'])
-
-# reorder columns
-df = df[['userid', 'day', 'hour', 'min', 'what', 'touches', 'apps', 'notification', 'age',
-       'gender', 'department', 'degree', 'Extraversion', 'Agreeableness',
-       'Conscientiousness', 'Neuroticism', 'Openness']]
-
+df = pd.read_pickle(data_folder + 'study_analysis.pkl')
 
 
 # %%
 # daily share of activities, globally users study 11% of their time
 # and 5.5 percent they are at at clas 
-
 df['what'].value_counts()/ sum(df['what'].value_counts())
 
 
 
 #%%
+''' STUDY SHARE ''' 
+# % of time students study, remove people who study 100% or 0% of the time
+
 # group by user and get global share of study/workgroup
 user_share = df.groupby('userid')['what'].value_counts()/df.groupby('userid')['what'].count()
 
@@ -45,24 +41,62 @@ user_share['total'] = user_share['Study/workgroup'] + user_share['Work/Class']
 user_share = user_share[(user_share['total'] != 0) & (user_share['total'] != 1)]
 df = df[df['userid'].isin(user_share.index)]
 #%%
+'''GET ONLY STUDY TIME'''
+
 df['what'].value_counts()
-
-
 df = df[df['what'] == 'Study/workgroup']
 
 
+#%%
+'''ONE STUDENT'''
+# user 128 on date 2020-11-30
 
-# drop user 232
-# %%# get only 30 november data 
+
+
 sig128 = df[df['userid'] == 128]
-sig128_study = sig128[sig128['what'] == 'Study/workgroup']
-sig128_study = sig128_study[sig128_study['day'] == '2020-11-30']
+sig128 = sig128[sig128['day'] == '2020-11-30']
 
+# make 0 nan 
+sig128['touches'] = sig128['touches'].replace(0, np.nan)
 
-# this but for every user  
+sig128.count()
 
+# sig 128 on day studied n minutes 
+study_time = sig128.count()['min']
+notification = sig128.count()['notification']
+touches = sig128.count()['touches']
+touches_and_notific = sig128[sig128['touches'] >0].count()['notification']
+most_used_app = sig128['apps'].value_counts().index[0]
+most_used_app_time = sig128['apps'].value_counts()[0]
+most_used_app2 = sig128['apps'].value_counts().index[1]
+most_used_app_time2 = sig128['apps'].value_counts()[1]
+most_used_app3 = sig128['apps'].value_counts().index[2]
+most_used_app_time3 = sig128['apps'].value_counts()[2]
 
-    
+most_notific = sig128['notification'].value_counts().index[0]
+most_notific_time = sig128['notification'].value_counts()[0]
+most_notific2 = sig128['notification'].value_counts().index[1]
+most_notific_time2 = sig128['notification'].value_counts()[1]
+most_notific3 = sig128['notification'].value_counts().index[2]
+most_notific_time3 = sig128['notification'].value_counts()[2]
+
+# this as a dataframe row 
+df_students = pd.DataFrame({'userid': 128, 'study_time': study_time, 'notification': notification, 'touches': touches, 'touches_and_notific': touches_and_notific, 'most_used_app': most_used_app, 'most_used_app_time': most_used_app_time, 'most_used_app2': most_used_app2, 'most_used_app_time2': most_used_app_time2, 'most_used_app3': most_used_app3, 'most_used_app_time3': most_used_app_time3, 'most_notific': most_notific, 'most_notific_time': most_notific_time, 'most_notific2': most_notific2, 'most_notific_time2': most_notific_time2, 'most_notific3': most_notific3}, index=[0])
+
+# this but for all students 
+df_students = pd.DataFrame(columns=['userid', 'study_time', 'notification', 'touches', 'touches_and_notific', 'most_used_app', 'most_used_app_time', 'most_used_app2', 'most_used_app_time2', 'most_used_app3', 'most_used_app_time3', 'most_notific', 'most_notific_time', 'most_notific2', 'most_notific_time2', 'most_notific3'])
+df_students = df.set_index('userid')
+df_students = df.groupby(['userid', 'day']).apply(lambda x: pd.Series({'study_time': x.count()['min'], 'notification': x.count()['notification'], 'touches': x.count()['touches'], 'touches_and_notific': x[x['touches'] >0].count()['notification'], 'most_used_app': x['apps'].value_counts().index[0], 'most_used_app_time': x['apps'].value_counts()[0], 'most_used_app2': x['apps'].value_counts().index[1], 'most_used_app_time2': x['apps'].value_counts()[1], 'most_used_app3': x['apps'].value_counts().index[2], 'most_used_app_time3': x['apps'].value_counts()[2], 'most_notific': x['notification'].value_counts().index[0], 'most_notific_time': x['notification'].value_counts()[0], 'most_notific2': x['notification'].value_counts().index[1], 'most_notific_time2': x['notification'].value_counts()[1], 'most_notific3': x['notification'].value_counts().index[2]}))
+
+## in df_students we have for each studet for each day some statistics about the study that day 
+
+# average study time
+
+#%%
+'''PHONE TOUCH FREQUENCY'''
+# in the following part of code  i compute the average numebr of minute 
+# students do not touch the phone when they claim thay are studying
+
 
 # from df_study compute the average lenght of streak of 0 in the touches column for each user 
 df_grouped = df.groupby('userid')
@@ -84,24 +118,29 @@ for studentid, group in df_grouped:
             nophone.append(count)
             count = 0
     # calculate the mean of the counter values
-    average = np.mean(nophone)
-    median = np.median(nophone)
-    std = np.std(nophone)
+    try: 
+        average = np.mean(nophone)
+        median = np.median(nophone)
+        std = np.std(nophone)
+        max_streak = np.max(nophone)
+    except:
+        average = 0
+        median = 0
+        std = 0
+        max_streak = 0
     # store the result in the dictionary
-    students[studentid] = {'average': average, 'median': median, 'std': std}
-    print('mean: ', average, 'median: ', median, 'std: ', std)
+
+
+    students[studentid] = {'average': average, 'median': median, 'std': std, 'max_streak': max_streak, 'nophone': nophone}
+    print('mean: ', average, 'median: ', median, 'std: ', std, 'max_streak: ', max_streak)
 
 
 ## add to user share 
 stats_phone = pd.DataFrame.from_dict(students, orient='index')
-
 user_share = user_share.join(stats_phone)
-
 rename = {'Study/workgroup': 'study', 'Work/Class': 'class', 
             'average': 'average_no_phone', 'median': 'median_no_phone', 'std': 'std_no_phone'}
-
 user_share = user_share.rename(columns=rename)
-
 #save csv 
 user_share.to_csv(data_folder + 'study_stats.csv')
 # %%
@@ -114,3 +153,32 @@ user_share = user_share.dropna()
 user_share = user_share.drop([112, 86])
 
 # %%
+
+all_nophone = [item for sublist in students.values() for item in sublist['nophone']]
+# get only data < 60
+some_nophone = [x for x in all_nophone if x < 60]
+
+plt.figure(figsize=(10, 5))
+plt.hist(some_nophone, bins=60)
+plt.title('Distribution of the number of minutes without phone')
+plt.xlabel('minutes')
+plt.ylabel('frequency')
+plt.savefig(plot_folder + 'no_touches_streak_lt60.png')
+
+
+
+# %%
+all_nophone = [item for sublist in students.values() for item in sublist['nophone']]
+
+# plot with log on x 
+plt.figure(figsize=(10, 5))
+plt.hist(all_nophone, bins=60, log=True)
+plt.title('Distribution of the number of minutes without phone for student 1')
+plt.xlabel('minutes')
+plt.ylabel('frequency')
+plt.savefig(plot_folder + 'plots/no_touches_streak_log.png')
+
+# %%
+# get all phone quantities
+
+
